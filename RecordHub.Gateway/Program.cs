d39124fb5985973e540b74a2acf5eeb1a34fb507
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-06-14 14:11:17 +0300

    Gateway: configure ocelot.json.

---
+++ RecordHub.Gateway/Program.cs
@@ -0,0 +1,20 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+var app = builder.Build(); app.UseRouting();
+app.UseRouting();
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = app.Configuration["Ocelot:PathToSwaggerGen"];
+});
+await app.UseOcelot();
+
+app.Run();

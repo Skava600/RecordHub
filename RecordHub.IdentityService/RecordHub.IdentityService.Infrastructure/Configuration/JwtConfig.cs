0d349e9ec1dbb550e569b99379ba335dc9f19ccf
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-06-14 14:42:32 +0300

    fix identity service with default key.

--- RecordHub.IdentityService/RecordHub.IdentityService.Infrastructure/Configuration/JwtConfig.cs
+++ RecordHub.IdentityService/RecordHub.IdentityService.Infrastructure/Configuration/JwtConfig.cs
@@ -4,6 +4,6 @@
     {
         public string Issuer { get; set; } = "RecordHub.IdentityService"; // издатель токена
         public string Audience { get; set; } = "Client"; // потребитель токена
-        public string Key { get; set; }   // ключ для шифрации
+        public string Key { get; set; } = "mysecretkey123";  // ключ для шифрации
     }
 }

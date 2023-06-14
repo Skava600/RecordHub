4fb4058db11058a41ccbeb9cdd872d974e65937a
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-06-14 14:10:36 +0300

    Mail Service: implement mailkit sending email.

---
+++ RecordHub.MailService/RecordHub.MailService.Application/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using RecordHub.MailService.Domain.Models;
+
+namespace RecordHub.MailService.Application.Services
+{
+    public interface IMailService
+    {
+        Task<bool> SendAsync(MailData mailData, CancellationToken ct);
+    }
+}

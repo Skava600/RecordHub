6bc9258f3f40b603820aae53b5982c491627b6d8
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-06-20 09:58:03 +0300

    Add catalog service to compose and fix audience in jwt.

--- RecordHub.CatalogService/RecordHub.CatalogService.Api/Program.cs
+++ RecordHub.CatalogService/RecordHub.CatalogService.Api/Program.cs
@@ -1,5 +1,6 @@
 using RecordHub.CatalogService.Api;
 using RecordHub.CatalogService.Api.Middlewares;
+using RecordHub.CatalogService.Infrastructure;
 using RecordHub.CatalogService.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,7 +22,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
 
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
--- RecordHub.Shared/Config/JwtConfig.cs
+++ RecordHub.Shared/Config/JwtConfig.cs
@@ -3,7 +3,7 @@
     public class JwtConfig
     {
         public string Issuer { get; set; } = "RecordHub.IdentityService"; // издатель токена
-        public string Audience { get; set; } = "Catalog"; // потребитель токена
+        public string Audience { get; set; } = "Client"; // потребитель токена
         public string Key { get; set; } = "mysecretkey123";  // ключ для шифрации
     }
 }

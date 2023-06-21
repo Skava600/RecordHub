462b7696b468cd8757f9cacda70b01dc3b6a84c6
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-06-21 17:17:08 +0300

    fix logging config.

--- RecordHub.MailService/RecordHub.MailService.Infrastructure/Config/LoggingConfig.cs
+++ RecordHub.MailService/RecordHub.MailService.Infrastructure/Config/LoggingConfig.cs
@@ -1,28 +1,24 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
-using Serilog.Events;
-
-namespace RecordHub.MailService.Infrastructure.Config
+﻿namespace RecordHub.MailService.Infrastructure.Config
 {
     public static class LoggingConfiguration
     {
+
         public static void ConfigureSerilog(this WebApplicationBuilder builder)
         {
             builder.Logging.ClearProviders();
-            builder.Host.UseSerilog((host, log) =>
-             {
-                 if (host.HostingEnvironment.IsProduction())
-                     log.MinimumLevel.Information();
-                 else
-                     log.MinimumLevel.Debug();
-
-                 log.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
-                 log.MinimumLevel.Override("Quartz", LogEventLevel.Information);
-                 log.WriteTo.Console();
-                 log.ReadFrom.Configuration(builder.Configuration);
-             });
+            LogEventLevel logLevel;
+            if (builder.Environment.IsProduction())
+                logLevel = LogEventLevel.Information;
+            else
+                logLevel = LogEventLevel.Debug;
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Quartz", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .MinimumLevel.Is(logLevel)
+                .WriteTo.Console(restrictedToMinimumLevel: logLevel)
+                .ReadFrom.Configuration(builder.Configuration);
+            builder.Logging.AddSerilog(log.CreateLogger(), false);
         }
     }
 }

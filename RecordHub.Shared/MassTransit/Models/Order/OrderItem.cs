e3b78bc41eece04745c27df6973e78e46f70db93
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-06-30 15:49:11 +0300

    Update shared lib.

---
+++ RecordHub.Shared/MassTransit/Models/Order/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordHub.Shared.MassTransit.Models.Order
+{
+    public class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}

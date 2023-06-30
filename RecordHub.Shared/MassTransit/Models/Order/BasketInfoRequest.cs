1ef8e7e4cfc0507db55847243ef564f238155b1f
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-06-30 15:49:11 +0300

    Update shared lib.

---
+++ RecordHub.Shared/MassTransit/Models/Order/BasketInfoRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace RecordHub.Shared.MassTransit.Models.Order
+{
+    public class BasketInfoRequest
+    {
+        public string UserId { get; set; }
+    }
+}
---
+++ RecordHub.Shared/MassTransit/Models/Order/BasketInfoResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordHub.Shared.MassTransit.Models.Order
+{
+    public class BasketInfoResponse
+    {
+        public string UserId { get; set; }
+        public IEnumerable<OrderItem> Items { get; set; }
+        public double Price { get; set; }
+
+    }
+}

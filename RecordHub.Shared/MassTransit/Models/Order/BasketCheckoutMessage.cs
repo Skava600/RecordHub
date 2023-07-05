b19fcfa7816cd4f942974d5af4351b21212c784b
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-07-05 14:33:53 +0300

    add shared lib to mail service branch.

---
+++ RecordHub.Shared/MassTransit/Models/Order/BasketCheckoutMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordHub.Shared.MassTransit.Models.Order
+{
+    public class BasketCheckoutMessage : IntegrationBaseEvent
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string UserId { get; set; }
+        public IEnumerable<OrderItemModel> Items { get; set; }
+        public double TotalPrice { get; set; }
+        public string Address { get; set; }
+    }
+}

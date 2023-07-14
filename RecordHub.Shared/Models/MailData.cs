5f3c9baf5433c5eae1034b8f613dbe66d9f81b7d
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-07-14 16:41:57 +0300

    Updated Shared files.

--- RecordHub.Shared/Models/MailData.cs
+++ RecordHub.Shared/Models/MailData.cs
@@ -2,11 +2,9 @@
 {
     public class MailData
     {
+
         // Receiver
         public List<string> To { get; }
-        public List<string> Bcc { get; }
-
-        public List<string> Cc { get; }
 
         // Sender
         public string? From { get; }
@@ -22,13 +20,17 @@
 
         public string? Body { get; }
 
-        public MailData(List<string> to, string subject, string? body = null, string? from = null, string? displayName = null, string? replyTo = null, string? replyToName = null, List<string>? bcc = null, List<string>? cc = null)
+        public MailData(
+            List<string> to,
+            string subject,
+            string? body = null,
+            string? from = null,
+            string? displayName = null,
+            string? replyTo = null,
+            string? replyToName = null)
         {
             // Receiver
             To = to;
-            Bcc = bcc ?? new List<string>();
-            Cc = cc ?? new List<string>();
-
             // Sender
             From = from;
             DisplayName = displayName;

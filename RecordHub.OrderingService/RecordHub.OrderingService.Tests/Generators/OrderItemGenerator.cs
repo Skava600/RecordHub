b81879a711c830ed73c4507f39110b36472534eb
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-07-31 19:28:39 +0300

    Ordering service: implement integration tests for controllers and consumers, unit tests for services and exception handler.

---
+++ RecordHub.OrderingService/RecordHub.OrderingService.Tests/Generators/OrderItemGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using RecordHub.OrderingService.Domain.Entities;
+
+namespace RecordHub.OrderingService.Tests.Generators
+{
+    public class OrderItemGenerator : Faker<OrderItem>
+    {
+        public OrderItemGenerator()
+        {
+            RuleFor(o => o.Quantity, f => f.Random.Int(1, 10));
+            RuleFor(o => o.Price, f => f.Random.Double(10, 100));
+            RuleFor(o => o.ProductId, f => Guid.NewGuid().ToString());
+            RuleFor(o => o.ProductName, f => f.Commerce.ProductName());
+        }
+    }
+}
---
+++ RecordHub.OrderingService/RecordHub.OrderingService.Tests/UnitTests/Middlewares/GlobalExceptionHandlingMiddlewareTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using RecordHub.OrderingService.Api.Middlewares;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace RecordHub.OrderingService.Tests.UnitTests.Middlewares
+{
+    public class GlobalExceptionHandlingMiddlewareTests
+    {
+        [Fact]
+        public async Task Invoke_Exception_InternalServerError()
+        {
+            // Arrange
+            var mockNext = new Mock<RequestDelegate>();
+            var mockLogger = new Mock<ILogger<GlobalExceptionHandlingMiddleware>>();
+            var context = CreateMockHttpContext();
+            var middleware = new GlobalExceptionHandlingMiddleware(mockNext.Object, mockLogger.Object);
+
+            var exceptionMessage = "Some error occurred.";
+            mockNext.Setup(next => next(context)).Throws(new Exception(exceptionMessage));
+
+            // Act
+            await middleware.Invoke(context);
+
+            // Assert
+            context.Response.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+
+            var responseBody = await GetResponseBody(context.Response);
+            responseBody.Should().Be(JsonSerializer.Serialize(exceptionMessage));
+        }
+
+        private HttpContext CreateMockHttpContext()
+        {
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+            return context;
+        }
+
+        private async Task<string> GetResponseBody(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+            using var reader = new StreamReader(response.Body, Encoding.UTF8);
+            return await reader.ReadToEndAsync();
+        }
+    }
+}

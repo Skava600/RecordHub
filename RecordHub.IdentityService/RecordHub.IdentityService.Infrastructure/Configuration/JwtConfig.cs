5abf7981f5c1bee9bfc61bbfe804b8344c33a5b4
Skava600 <56450477+Skava600@users.noreply.github.com>
2023-08-08 13:51:25 +0300

    fix identity tests in github.

--- RecordHub.IdentityService/RecordHub.IdentityService.Infrastructure/Configuration/JwtConfig.cs
+++ RecordHub.IdentityService/RecordHub.IdentityService.Infrastructure/Configuration/JwtConfig.cs
@@ -2,9 +2,9 @@
 {
     public class JwtConfig
     {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-        public string Key { get; set; }
+        public string Issuer { get; set; } = "identity";
+        public string Audience { get; set; } = "client";
+        public string Key { get; set; } = "SecretKey";
         public int TokenLifeTime { get; set; }
     }
 }
